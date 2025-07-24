196e6cd7ebbdd318183fe694325f16a86c24a499
shoikot <saikotju@hotmail.com>
2025-07-24 17:23:58 +0600

    chages to resolve main.jsx

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// vite.config.js
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-    base: '/e-plantShopping/',
-    plugins: [react()]
+    plugins: [react()],
+    base: '/e-plantShopping/' // <-- MUST match your GitHub repo name
 });
